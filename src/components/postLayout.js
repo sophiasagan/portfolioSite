c63179b0b1eb56456f4d4b859e11a011b793d481
Sophia Braddock <sophiabraddock82@gmail.com>
2020-11-19 09:32:10 -0500

    working on comments

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,6 +1,8 @@
-import React, { Component } from "react"
+import React, { Component, useEffect } from "react"
 import { graphql } from "gatsby"
 import Layout from "./layout"
+import Comment from '../components/comment'
+
 
 // Static Query
 // Used anywhere, doesn't accept variable, can't use context
@@ -8,10 +10,35 @@ import Layout from "./layout"
 // Page Query
 // Must be used on pages
 
+const commentBox = React.createRef()
+useEffect(() => {
+  const commentScript = document.createElement('script')
+  const theme =
+    typeof window !== 'undefined' && localStorage.getItem('theme') === 'dark'
+      ? 'github-dark'
+      : 'github-light'
+  commentScript.async = true
+  commentScript.src = 'https://utteranc.es/client.js'
+  commentScript.setAttribute('repo', 'sophiasagan/comments') // CHANGE THIS if you're just going to clone this repo and use the code. Do not test your code using my repo.
+  commentScript.setAttribute('issue-term', 'pathname')
+  commentScript.setAttribute('id', 'utterances')
+  commentScript.setAttribute('theme', theme)
+  commentScript.setAttribute('crossorigin', 'anonymous')
+  if (commentBox && commentBox.current) {
+    commentBox.current.appendChild(commentScript)
+  } else {
+    console.log(`Error adding utterances comments on: ${commentBox}`)
+  }
+}, [])
+
 export default class postLayout extends Component {
   render() {
     const { markdownRemark } = this.props.data
     const { location } = this.props
+    // const commentBox = React.createRef()
+
+  
+
     return (
       <Layout location={location}>
         <h1>{markdownRemark.frontmatter.title}</h1>
@@ -20,17 +47,12 @@ export default class postLayout extends Component {
             __html: markdownRemark.html,
           }}
         />
-        <section class="comments">
-        <script
-          src="https://utteranc.es/client.js"
-          repo="sophiasagan/portfolioSite"
-          issue-term="pathname"
-          label="✨💬✨"
-          theme="github-light"
-          crossorigin="anonymous"
-          async
-        ></script>
-        </section>
+        <div className="container">
+        <div id="comments">
+          <h2>Comments</h2>
+          <Comment commentBox={commentBox} />
+        </div>
+        </div>
       </Layout>
     )
   }
